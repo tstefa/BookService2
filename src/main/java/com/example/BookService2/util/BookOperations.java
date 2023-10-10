a5dc1a9bcce4fda7968cf7d6c346169f9af2f075
Tihomir Stefanov <Tihomir.Stefanov2@aexp.com>
2023-10-10 12:27:50 +0100

    Updated the saving method to the file

--- src/main/java/com/example/BookService2/util/BookOperations.java
+++ src/main/java/com/example/BookService2/util/BookOperations.java
@@ -4,12 +4,8 @@ import com.example.BookService2.model.BookDao;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,10 +16,8 @@ public class BookOperations{
 
 
     public List<BookDao> jsonToBookDao() throws IOException {
-        Resource classPathResource = new ClassPathResource(Constants.FILE_PATH);
-        File file = classPathResource.getFile();
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.readValue(file,new TypeReference<List<BookDao>>(){});
+        return objectMapper.readValue(new File(Constants.FILE_PATH), new TypeReference<List<BookDao>>(){});
     }
 
     public List<String> convertBooksToString(List<BookDao> bookDaos){
@@ -31,35 +25,10 @@ public class BookOperations{
     }
 
     public List<BookDao> addBook(BookDao bookDao,List<BookDao> bookDaoList) throws IOException {
-
-//        JSONObject jsonObject = new JSONObject(bookDaoList);
         bookDaoList.add(bookDao);
-        try (FileWriter file = new FileWriter(Constants.FILE_PATH)) {
-            JSONObject jsonObject = new JSONObject(bookDaoList.toString());
-
-            log.info("Writing file ...");
-            file.write(bookDaoList.toString());
-            log.info("File written successfully!");
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-////            InputStream inputStream = new FileInputStream(new File("/books.json"));
-////            TypeReference<List<BookDao>> typeReference = new TypeReference<List<BookDao>>() {};
-////            List<BookDao> bookDaos = objectMapper.readValue(inputStream,typeReference);
-//            bookDaoList.add(bookDao);
-//
-//            objectMapper.writeValue(new File("/books.json"),bookDaoList);
-//        }catch (FileNotFoundException e){
-//            e.printStackTrace();
-//        }catch (JsonParseException e){
-//            e.printStackTrace();
-//        }
-
-//        FileWriter file = new FileWriter("/books.json");
-//        file.append(bookDao);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(new File(Constants.FILE_PATH),bookDaoList);
+        log.info("Updated file: ");
         return bookDaoList;
     }
 
