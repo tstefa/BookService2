6a40737becb36d52b1f66d06a03ae4a0fbd00a92
Tihomir Stefanov <Tihomir.Stefanov2@aexp.com>
2023-10-11 18:28:44 +0100

    Fixed connection issue between the two services

--- src/main/java/com/example/BookService2/route/BookRoute.java
+++ src/main/java/com/example/BookService2/route/BookRoute.java
@@ -3,28 +3,36 @@ package com.example.BookService2.route;
 import com.example.BookService2.model.BookDao;
 import com.example.BookService2.processor.BookAddition;
 import com.example.BookService2.processor.BookProcessor;
-import com.example.BookService2.util.JsonConverter;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookRoute extends RouteBuilder {
-
-    private final JsonConverter jsonConverter = new JsonConverter();
     private final JacksonDataFormat bookDataFormat = new JacksonDataFormat(BookDao.class);
     @Override
-    public void configure() throws Exception {
+    public void configure() {
         from("direct:book").process(new BookProcessor());
 
         from("direct:addBook")
                 .process(new BookAddition());
 
         from("direct:loanBook")
-                .bean(BookProcessor.class,"getBook")
+                .bean(BookProcessor.class, "getBook")
+                .log("${body}")
                 .marshal(bookDataFormat)
-                .to("rest:post:https://localhost:8081/bookStore/loanBook/");
+                .log("${body}")
+                .to("http://localhost:8083/bookStore/loanBook");
 
 //        EXPERIMENTAL
+//        from("timer: first-timer")
+//                .transform().constant("Constant")
+//                .to("log:first-timer");
+//    }
+
+//    If I want an operation that does not make a change on the body you use processing
+//    If you want to have an operation that makes changes to the body of the message
+
+//    Working with ActiveMQ - What is it - are we using it as well
     }
 }
