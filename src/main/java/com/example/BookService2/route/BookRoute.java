46b13778caa8bc4934805e7cc288b082487faa43
Tihomir Stefanov <Tihomir.Stefanov2@aexp.com>
2023-10-11 15:15:24 +0100

    Trying to create an endpoint to endpoint connection to update cutomer with a book they have loaned. Still work in progress and there is a bean creatino error.

--- src/main/java/com/example/BookService2/route/BookRoute.java
+++ src/main/java/com/example/BookService2/route/BookRoute.java
@@ -1,16 +1,30 @@
 package com.example.BookService2.route;
 
+import com.example.BookService2.model.BookDao;
 import com.example.BookService2.processor.BookAddition;
 import com.example.BookService2.processor.BookProcessor;
+import com.example.BookService2.util.JsonConverter;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BookRoute extends RouteBuilder {
+
+    private final JsonConverter jsonConverter = new JsonConverter();
+    private final JacksonDataFormat bookDataFormat = new JacksonDataFormat(BookDao.class);
     @Override
     public void configure() throws Exception {
         from("direct:book").process(new BookProcessor());
 
-        from("direct:addBook").process(new BookAddition());
+        from("direct:addBook")
+                .process(new BookAddition());
+
+        from("direct:loanBook")
+                .bean(BookProcessor.class,"getBook")
+                .marshal(bookDataFormat)
+                .to("rest:post:https://localhost:8081/bookStore/loanBook/");
+
+//        EXPERIMENTAL
     }
 }
